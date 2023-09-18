b6d8ad38ef12ddb88f9332946b9c7bd1a64a68f3
Choor Hong <abner.hong@hotmail.com>
2023-09-18 14:22:23 +0800

    Restructure handlers

--- src/mocks/utils/index.ts
+++ src/mocks/utils/index.ts
@@ -1,73 +1,78 @@
 import { ItemType } from "../interfaces";
 import {
-  // categorizeCarparkLotSize,
-  // combineCarparkData,
-  // retrieveCpMinMaxAvailability,
+  categorizeCarparkLotSize,
+  combineCarparkData,
+  retrieveCpMinMaxAvailability,
   calculateCarparkMinMaxAvailability,
   combineAndCategorizeCarparkData,
 } from "./helpers";
 
-export const retrieveCarparkCategoryData = (items: ItemType[]) => {
-  // // ----- OLD HANDLER 1 : -----
-  // // ----- NO LONGER WORKS WITH FRONTEND -----
+/**
+ * Deprecated!. This handler no longer integrates with FE.
+ * @param items
+ * @returns
+ */
+export const getCpCategoryDataV1 = async (items: ItemType[]) => {
+  // Combine the total lots of a carpark
+  const combinedCarparkData = combineCarparkData(items);
+  // Categorize the carpark based on their total lots
+  const categories = categorizeCarparkLotSize(
+    combinedCarparkData[0].carpark_data
+  );
+  // Retrieve the highest and lowest lots availability of a category
+  const lotSizeInfo = retrieveCpMinMaxAvailability(categories);
+  return lotSizeInfo;
+};
 
-  // // Combine the total lots of a carpark
-  // const combinedCarparkData = combineCarparkData(items);
+/**
+ * Old handler. Please use [getCpCategoryDataV3] for an logically optimized handler.
+ * @param items
+ * @returns
+ */
+export const getCpCategoryDataV2 = async (items: ItemType[]) => {
+  // Combine the total lots of a carpark
+  const combinedCarparkData = combineCarparkData(items);
 
-  // // Categorize the carpark based on their total lots
-  // const categories = categorizeCarparkLotSize(
-  //   combinedCarparkData[0].carpark_data
-  // );
+  // Categorize the carpark based on their total lots
+  const categorizeCpLotSize = (items: typeof combinedCarparkData) => {
+    return items.map((item) => {
+      const categorizedCarparkLotSize = categorizeCarparkLotSize(
+        item.carpark_data
+      );
+      return {
+        timestamp: item.timestamp,
+        categorized_carpark_data: categorizedCarparkLotSize,
+      };
+    });
+  };
+  const categorizedCpLotSize = categorizeCpLotSize(combinedCarparkData);
 
-  // // Retrieve the highest and lowest lots availability of a category
-  // const lotSizeInfo = retrieveCpMinMaxAvailability(categories);
-  // return lotSizeInfo;
+  // Calculate and retrieve the highest and lowest lots availability of a category
+  const result = calculateCarparkMinMaxAvailability(categorizedCpLotSize);
+  return result;
+};
 
-  // // ----- OLD HANDLER 1 : -----
-  // // ----- NO LONGER WORKS WITH FRONTEND -----
-
-  // // ----- OLD HANDLER 2: -----
-  // // ----- WORKS WITH FRONTEND -----
-
-  // // Combine the total lots of a carpark
-  // const combinedCarparkData = combineCarparkData(items);
-
-  // const categorizeCpLotSize = (items: typeof combinedCarparkData) => {
-  //   return items.map((item) => {
-  //     const categorizedCarparkLotSize = categorizeCarparkLotSize(
-  //       item.carpark_data
-  //     );
-
-  //     return {
-  //       timestamp: item.timestamp,
-  //       categorized_carpark_data: categorizedCarparkLotSize,
-  //     };
-  //   });
-  // };
-
-  // // Categorize the carpark based on their total lots
-  // const categorizedCpLotSize = categorizeCpLotSize(combinedCarparkData);
-
-  // // Calculate and retrieve the highest and lowest lots availability of a category
-  // const result = calculateCarparkMinMaxAvailability(categorizedCpLotSize);
-
-  // // ----- OLD HANDLER 2: -----
-  // // ----- WORKS WITH FRONTEND -----
-
-  // // ----- LATEST HANDLER 3: -----
-  // // ----- WORKS WITH FRONTEND -----
-
-  // // Combine and categorize the carpark based on their total lots
+const getCpCategoryDataV3 = async (items: ItemType[]) => {
+  //  Combine and categorize the carpark based on their total lots
   const combinedAndCategorizedCarparkData =
     combineAndCategorizeCarparkData(items);
 
-  // // Calculate and retrieve the highest and lowest lots availability of a category
+  //  Calculate and retrieve the highest and lowest lots availability of a category
   const result = calculateCarparkMinMaxAvailability(
     combinedAndCategorizedCarparkData
   );
+  return result;
+};
+
+export const retrieveCarparkCategoryData = async (items: ItemType[]) => {
+  // // ----- OLD HANDLER 1 : -----
+  // const result = getCpCategoryDataV1(items);
+
+  // // ----- OLD HANDLER 2: -----
+  // const result = getCpCategoryDataV2(items);
 
   // // ----- LATEST HANDLER 3: -----
-  // // ----- WORKS WITH FRONTEND -----
+  const result = getCpCategoryDataV3(items);
 
   return result;
 };
